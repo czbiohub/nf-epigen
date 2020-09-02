fe6539c997c6ec883b95b336ebdec481ab08290b
phoenixAja <phoenix@phoenixlogan.net>
2020-09-02 15:04:08 -0700

    step for download jsons

--- bin/download_data_czb.sh
+++ bin/download_data_czb.sh
@@ -2,9 +2,13 @@
 
 mkdir newicks
 mkdir gisaid
+mkdir jsons
 
 # download newicks
 aws s3 cp s3://czb-covid-results/gisaid/results/ newicks/ --recursive --exclude '*' --include '*.nwk'
+
+# download jsons
+aws s3 cp s3://czb-covid-results/gisaid/results/ jsons/ --recursive --exclude '*' --include '*.json'
 
 # download gisaid sequences and metadata
 aws s3 cp s3://czb-covid-results/gisaid/metadata.tsv.gz - | gunzip -cq >gisaid/metadata.tsv
